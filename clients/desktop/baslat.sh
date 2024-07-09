483bdb3a2083632b0ed54ada96be3ed4d9871d56
OguzhanTekeli06 <213301042.ogr.selcuk.edu.tr>
2024-07-09 14:13:35 +0300

    Klasör düzenlemesi

--- clients/desktop/baslat.sh
+++ clients/desktop/baslat.sh
@@ -1,7 +1,4 @@
-
-
-
 #!/bin/bash
 source source /home/oguzhan/calculator/venv/bin/activate
-python3 /home/oguzhan/calculator/interface/app.py
+python3 /home/oguzhan/calculator/clients/desktop/app.py
 
